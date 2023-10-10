7b488b474b9cf89777858888528bdc038b1247d6
mardingit <githubmardinsaedmocheshi@gmail.com>
2023-10-11 00:22:13 +0330

    Message settings to display in the admin panel | تنظیمات پیغام برای نمایش در پنل ادمین

--- MainPage/admin.py
+++ MainPage/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AlertModelMainPage
 
-# Register your models here.
+
+
+
+# تنظیمات پیغام
+class AlertMainPageAdmin(admin.ModelAdmin):
+    list_display = ('title_alert' , 'slug_alert', 'status_alert', 'updated_alert', 'created_alert')
+    list_filter = (['publish_alert' ,'status_alert'] )
+    search_fields = ('title_alert' ,'description_alert'  )
+    prepopulated_fields = {'slug_alert': ('title_alert',)}
+    ordering = ['-publish_alert' , 'status_alert']
+
+admin.site.register(AlertModelMainPage, AlertMainPageAdmin)
+admin.site.title_article = 'مدیریت پیغام ها'
+# --------------------------------------------------
