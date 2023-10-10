75ea368c45524e4d30ef46171774afc8f20a74ce
mardingit <githubmardinsaedmocheshi@gmail.com>
2023-10-11 00:08:52 +0330

    Add home page message model | اضافه کردن مدل پیغام صفحه اصلی

--- MainPage/models.py
+++ MainPage/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+# Alert | پیغام
+class AlertModelMainPage(models.Model):
+  STATUS_CHOICES = (
+    ('d','پیش نویس'),
+    ('p','منتشر'),
+    ('f','پیغام فاقد محتوا'),
+    ('n','حذف خواهد شد'),
+    ('b','به زودی منتشر خواهد شد'),
+    ('r','به دلیل عمل نکردن به قوانین سایت رد شد'),
+  )
+  Alert_CHOICES= [
+   ("نوع پیغام", (
+           ("hhh", "هشدار"),
+           ("sss", "مهم"),
+           ("aaa", "پیغام تعمیر"),
+           ("qqq", "پیغام اضافه شدن ویژگی جدید"),
+           ("www", "پیغام تخفیف ویژه"),
+           ("eee", "پیغام کمک"),
+           ("rrr", "پیغام اطلاع رسانی"),
+           ("ttt", "پیغام انگیزشی"),
+           ("yyy", "پیغام تبلیغاتی"),
+           ("uuu", "پیغام ارتباط با ما"),
+           ("iii", "پیغام تبریک"),
+           ("ooo", " سایر "),
+
+
+       )
+   ),
+  ]
+
+  title_alert = models.CharField(max_length=200, verbose_name = "عنوان پیغام")
+  slug_alert = models.SlugField(max_length=100 , unique=True, verbose_name = "آدرس پیغام")
+#   category_alert = models.ManyToManyField("Category", verbose_name="دسته بندی", related_name="articls")
+  description_alert = models.TextField(verbose_name = "توضیح کوتاه")
+#  description_alert = RichTextField(blank=True, null=True,verbose_name = "توضیح کوتاه")
+  publish_alert = models.DateTimeField(default=timezone.now, verbose_name = "زمان انتشار پیغام")
+  created_alert = models.DateTimeField(auto_now_add=True, verbose_name = "پیغام کی ایجاد شد؟") 
+  updated_alert = models.DateTimeField(auto_now =True, verbose_name = "پیغام کی آپدیت شد؟")
+  status_alert = models.CharField(max_length=1,choices=STATUS_CHOICES,default = 'd', verbose_name = "  وضعیت انتشار پیغام" )
+  Alert_type_alert = models.CharField(max_length=3,choices=Alert_CHOICES, verbose_name = " نوع پیغام" )
+  Emphasis_on_the_message_alert = models.BooleanField(default=False, verbose_name = "تاکید به پیغام")
+
+  class Meta:
+    verbose_name_plural = "پیغام ها"
+    verbose_name = "پیغام"
+    
+  def __str__(self):
+    return self.title_alert
