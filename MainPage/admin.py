347b62e5120cc1dca84b4ab044256e68de8a0f8e
mardingit <githubmardinsaedmocheshi@gmail.com>
2023-10-11 11:30:37 +0330

    Book settings to display in admin panel | تنظیمات کتاب برای نمایش در پنل ادمین

--- MainPage/admin.py
+++ MainPage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AlertModelMainPage
+from .models import AlertModelMainPage, BookHomeModelMainPage
 
 
 
@@ -14,4 +14,18 @@ class AlertMainPageAdmin(admin.ModelAdmin):
 
 admin.site.register(AlertModelMainPage, AlertMainPageAdmin)
 admin.site.title_article = 'مدیریت پیغام ها'
+# --------------------------------------------------
+
+
+
+# تنظیمات کتاب صفحه اصلی
+class BookHomeMainPageAdmin(admin.ModelAdmin):
+    list_display = ('title_BookHome' , 'slug_BookHome', 'status_BookHome', 'updated_BookHome', 'created_BookHome')
+    list_filter = (['publish_BookHome' ,'status_BookHome'] )
+    search_fields = ('title_BookHome' ,'description_BookHome'  )
+    prepopulated_fields = {'slug_BookHome': ('title_BookHome',)}
+    ordering = ['-publish_BookHome' , 'status_BookHome']
+
+admin.site.register(BookHomeModelMainPage, BookHomeMainPageAdmin)
+admin.site.title_article = 'مدیریت کتاب ها'
 # --------------------------------------------------
