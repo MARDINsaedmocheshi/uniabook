c32712a2ad5c958c7f7cb46f663b896a9d7f19bc
mardingit <githubmardinsaedmocheshi@gmail.com>
2023-10-11 10:39:34 +0330

    Making the home page book | ساخت کتاب صفحه اصلی

--- MainPage/models.py
+++ MainPage/models.py
@@ -61,3 +61,73 @@ class AlertModelMainPage(models.Model):
     
   def __str__(self):
     return self.title_alert
+
+
+# ------------------------------------------------------------------------------------------------
+
+
+# Home page book | کتاب صفحه اصلی 
+class BookHomeModelMainPage(models.Model):
+  STATUS_CHOICES = (
+    ('d','پیش نویس'),
+    ('p','منتشر'),
+    ('p','تعداد کتاب محدود'),
+    ('f','کتاب موجود نیست'),
+    ('n','حذف خواهد شد'),
+    ('b','به زودی موجود خواهد شد'),
+    ('r','به دلیل عمل نکردن به قوانین سایت رد شد'),
+  )
+  BookHome_CHOICES= [
+   ("انواع کتاب بر اساس موضوع؛ انواع سبک کتاب", (
+
+           ("ban", "دانشگاهی"),
+           ("bao", "آموزشی"), 
+           ("bab", "فانتزی"),
+           ("bac", "ماجراجویی"),
+           ("bad", "عاشقانه "),
+           ("bae", "ترسناک"),
+           ("baf", "مهیج "),
+           ("bag", "علمی تخیلی"),
+           ("bah", "هنری"),
+           ("bai", "انگیزشی"),
+           ("baj", "طنز"),
+           ("bak", "کودکان"),
+           ("bal", "کتاب های زندگینامه"),
+           ("bam", "کتاب های سفرنامه"),
+
+
+       )
+   ),
+  ]
+
+  image_BookHome = models.ImageField(upload_to="BookHome_image", verbose_name = "عکس کتاب")
+  title_BookHome = models.CharField(max_length=200, verbose_name = "نام کتاب به طور کامل" )
+  slug_BookHome = models.SlugField(max_length=100 , unique=True, verbose_name = "آدرس کتاب")
+#   نام نویسنده
+  Publication_name_BookHome = models.CharField(max_length=200, verbose_name = "نام نشر" )
+  publication_date_BookHome = models.CharField(max_length=200, verbose_name = "تاریخ نشر" )
+  Place_of_publication_BookHome = models.CharField(max_length=200, verbose_name = "محل نشر" )
+  name_of_the_printing_house_BookHome = models.CharField(max_length=200, verbose_name = "نام چاپخانه" )
+  Number_of_circulation_BookHome = models.CharField(max_length=200, verbose_name = "تعداد تیراژ" )
+  Publishedbook_BookHome = models.CharField(max_length=200, verbose_name = "نوبت چاپ" )
+  price_of_the_book_BookHome = models.DecimalField(default=0, max_digits=12, decimal_places=0, verbose_name = "قیمت کتاب")
+  name_of_the_translator_or_translators_BookHome = models.CharField(max_length=200, verbose_name = "نام مترجم یا مترجمان" )
+#   category_BookHome = models.ManyToManyField("Category", verbose_name="دسته بندی", related_name="articls")
+  description_BookHome = models.TextField(verbose_name = "توضیحات کتاب")
+#  description_BookHome = RichTextField(blank=True, null=True,verbose_name = "توضیح کوتاه")
+  publish_BookHome = models.DateTimeField(default=timezone.now, verbose_name = "زمان انتشار کتاب")
+  created_BookHome = models.DateTimeField(auto_now_add=True, verbose_name = "کتاب کی ایجاد شد؟") 
+  updated_BookHome = models.DateTimeField(auto_now =True, verbose_name = "کتاب کی آپدیت شد؟")
+  status_BookHome = models.CharField(max_length=1,choices=STATUS_CHOICES,default = 'd', verbose_name = "  وضعیت انتشار کتاب" )
+  BookHome_type_BookHome = models.CharField(max_length=3,choices=BookHome_CHOICES, verbose_name = " نوع کتاب" )
+  is_sale_BookHome = models.BooleanField(default=False, verbose_name = "تخفیف ویژه")
+
+  class Meta:
+    verbose_name_plural = "کتاب ها"
+    verbose_name = "کتاب"
+    
+  def __str__(self):
+    return self.title_BookHome
+
+
+# ------------------------------------------------------------------------------------------------
