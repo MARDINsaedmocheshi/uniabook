5da95794e3ae965c3ba2f8c0b8a8b26ec0d4f2e8
mardingit <githubmardinsaedmocheshi@gmail.com>
2023-10-13 23:32:47 +0330

    Receiving information from the database using models and setting it in the view and displaying the objects on the page | دریافت اطلاعات از دیتابیس با استفاده از مدلها و تنظیم آن در ویو و نمایش آبجکت ها در صفحه

--- MainPage/views.py
+++ MainPage/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import AlertModelMainPage, BookHomeModelMainPage, ArticleHomeModelMainPage
 
 def MainPage_view(request):
-    # صفحه اصلی 
-    return render(request, "MainPage/index.html")
+    cotext = {
+
+        # پیغام ها
+         "Alertspagemain" : AlertModelMainPage.objects.filter(status_alert="p"), # شیش تای آخر رو نشون میده
+        # کتاب ها        
+         "Bookspagemain" : BookHomeModelMainPage.objects.filter(status_BookHome="p")[:6], # شیش تای آخر رو نشون میده
+        # مقاله ها        
+         "Articlspagemain" : ArticleHomeModelMainPage.objects.filter(status_ArticleHome="p")[:6], # شیش تای آخر رو نشون میده
+
+     }
+    return render(request, "MainPage/index.html", cotext)
