6e31e37147266129d8df2929d467117fcb039215
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-16 17:59:54 +0330

    A bug in the file views.py | یک باگ در فایل views.py

--- MainPage/views.py
+++ MainPage/views.py
@@ -6,6 +6,7 @@ def MainPage_view(request):
 
         # پیغام ها
          "Alertspagemain" : AlertModelMainPage.objects.filter(status_alert="p"), # شیش تای آخر رو نشون میده
+        #  "Alertspagemain" : AlertModelMainPage.objects.published(), # میتونی اینجوری هم بنویسی
         # کتاب ها        
          "Bookspagemain" : BookHomeModelMainPage.objects.filter(status_BookHome="p")[:6], # شیش تای آخر رو نشون میده
         # مقاله ها        
@@ -33,7 +34,9 @@ def detail_alert(request, slug):
         # پیغام ها
         "Alertspagemain" : AlertModelMainPage.objects.filter(status_alert="p"),
         "detail_alert" : AlertModelMainPage.objects.get(slug_alert=slug),
-        "category" : Category.objects.filter(status_category=True)
+        "category" : Category.objects.filter(status_category=True),
+        "DA" : get_object_or_404(AlertModelMainPage , slug_alert=slug , status_alert="p")
+        # "DA" : get_object_or_404(AlertModelMainPage.objects.published() , slug_alert=slug)# میتونی اینجوری هم بنویسی
     }
     return render(request, "MainPage/alert_detail.html", cotext)
 # -----------------------------------------------
@@ -45,7 +48,8 @@ def detail_book(request, slug):
         # کتاب ها 
         "Bookspagemain" : BookHomeModelMainPage.objects.filter(status_BookHome="p")[:6],
         "detail_book" : BookHomeModelMainPage.objects.get(slug_BookHome=slug),
-        "category" : Category.objects.filter(status_category=True)
+        "category" : Category.objects.filter(status_category=True),
+        "DB" : get_object_or_404(BookHomeModelMainPage , slug_BookHome=slug , status_BookHome="p")
     }
     return render(request, "MainPage/book_detail.html", cotext)
 # -----------------------------------------------
@@ -57,7 +61,8 @@ def detail_article(request, slug):
         # مقاله ها     
         "Articlspagemain" : ArticleHomeModelMainPage.objects.filter(status_ArticleHome="p")[:6],
         "detail_article" : ArticleHomeModelMainPage.objects.get(slug_ArticleHome=slug),
-        "category" : Category.objects.filter(status_category=True)
+        "category" : Category.objects.filter(status_category=True),
+        "DAR" : get_object_or_404(ArticleHomeModelMainPage , slug_ArticleHome=slug , status_ArticleHome="p")
     }
     return render(request, "MainPage/article_detail.html", cotext)
 # -----------------------------------------------
@@ -68,6 +73,7 @@ def category_Alert(request, slug):
     cotext = {
         # دسته بندی ها
         "category" : get_object_or_404(Category, slug_category=slug, status_category=True),
+        # "category" : get_object_or_404(Category.objects.published(), slug_category=slug),# میتونی اینجوری هم بنویسی
         # "category1" : Category.objects.filter(status_category=True)
     }
     return render(request, "MainPage/category_alerts.html", cotext)
