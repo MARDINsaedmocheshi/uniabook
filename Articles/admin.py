808fb32675118c130cc75739d444ca31dcaf4ac1
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-18 13:07:15 +0330

    Category settings in the file (admin.py) to display it in the admin panel | تنظیمات دسته بندی در فایل (admin.py) برای نمایش آن در پنل مدیریت

--- Articles/admin.py
+++ Articles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+# تنظیمات دسته بندی
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('position_category','title_category', 'slug_category' , 'parent', 'status_category','jpublish','created_category','updated_category')
+    list_filter = (['status_category'])
+    search_fields = ('title_category' ,'slug_category')
+    prepopulated_fields = {'slug_category': ('title_category',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.title_category = 'مدیریت دسته بندی'
+# --------------------------------------------------
