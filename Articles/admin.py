5f9474e842c7b47681f8e0e2233d79f3a5cbb6c2
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-18 19:56:20 +0330

    Writing the Alert model for the article application and setting it up for display in the management panel | نوشتن مدل پیغام برای اپلیکیشن مقالات و تنظیم آن برای نمایش در پنل مدیریتی

--- Articles/admin.py
+++ Articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category , AlertTop
 
 
 # تنظیمات دسته بندی
@@ -12,3 +12,28 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 admin.site.title_category = 'مدیریت دسته بندی'
 # --------------------------------------------------
+
+
+
+
+
+# تنظیمات پیغام
+class AlertAdmin(admin.ModelAdmin):
+    list_display = ('title_alert' , 'slug_alert', 'status_alert', 'jpublish', 'updated_alert', 'created_alert', 'category_to_str')
+    list_filter = (['publish_alert' ,'status_alert'] )
+    search_fields = ('title_alert' ,'description_alert'  )
+    prepopulated_fields = {'slug_alert': ('title_alert',)}
+    ordering = ['-publish_alert' , 'status_alert']
+
+    def category_to_str(self, obj):
+        return "،".join([category.title_category for category in obj.categore_published()])
+    category_to_str.short_description ="دسته بندی"
+
+admin.site.register(AlertTop, AlertAdmin)
+admin.site.title_article = 'مدیریت پیغام ها'
+# --------------------------------------------------
+
+
+
+
+
--- Articles/models.py
+++ Articles/models.py
@@ -34,3 +34,65 @@ class Category(models.Model):
 
   # objects = CategoryManagers()  
 # -----------------------------------------------
+
+
+
+# Alert | پیغام
+class AlertTop(models.Model):
+  STATUS_CHOICES = (
+    ('d','پیش نویس'),
+    ('p','منتشر'),
+    ('f','پیغام فاقد محتوا'),
+    ('n','حذف خواهد شد'),
+    ('b','به زودی منتشر خواهد شد'),
+    ('r','به دلیل عمل نکردن به قوانین سایت رد شد'),
+  )
+  Alert_CHOICES= [
+   ("نوع پیغام", (
+           ("hhh", "هشدار"),
+           ("sss", "مهم"),
+           ("aaa", "پیغام تعمیر"),
+           ("qqq", "پیغام اضافه شدن ویژگی جدید"),
+           ("www", "پیغام تخفیف ویژه"),
+           ("eee", "پیغام کمک"),
+           ("rrr", "پیغام اطلاع رسانی"),
+           ("ttt", "پیغام انگیزشی"),
+           ("yyy", "پیغام تبلیغاتی"),
+           ("uuu", "پیغام ارتباط با ما"),
+           ("iii", "پیغام تبریک"),
+           ("ooo", " سایر "),
+
+       )
+   ),
+  ]
+
+  image_alert = models.ImageField(null=True,blank=True, upload_to="alert_image", verbose_name = "عکس پیغام")
+  title_alert = models.CharField(max_length=200, verbose_name = "عنوان پیغام")
+  slug_alert = models.SlugField(max_length=100 , unique=True, verbose_name = "آدرس پیغام")
+  category_alert = models.ManyToManyField("Category", verbose_name="دسته بندی", related_name="alerts")
+  description_alert = models.TextField(verbose_name = "توضیح کوتاه")
+#  description_alert = RichTextField(blank=True, null=True,verbose_name = "توضیح کوتاه")
+  publish_alert = models.DateTimeField(default=timezone.now, verbose_name = "زمان انتشار پیغام")
+  created_alert = models.DateTimeField(auto_now_add=True, verbose_name = "پیغام کی ایجاد شد؟") 
+  updated_alert = models.DateTimeField(auto_now =True, verbose_name = "پیغام کی آپدیت شد؟")
+  status_alert = models.CharField(max_length=1,choices=STATUS_CHOICES,default = 'd', verbose_name = "  وضعیت انتشار پیغام" )
+  Alert_type_alert = models.CharField(max_length=3,choices=Alert_CHOICES, verbose_name = " نوع پیغام" )
+  Emphasis_on_the_message_alert = models.BooleanField(default=False, verbose_name = "تاکید به پیغام")
+
+  class Meta:
+    verbose_name_plural = "پیغام ها"
+    verbose_name = "پیغام"
+    
+  def __str__(self):
+    return self.title_alert
+
+  def jpublish(self):
+      return jalali_convert(self.publish_alert)
+  jpublish.short_description ="زمان انتشار"
+
+  def categore_published(self):
+    return self.category_alert.filter(status_category=True)
+
+#   objects = AlertManagers()
+
+# ------------------------------------------------------------------------------------------------
