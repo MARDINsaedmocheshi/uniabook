1246b162231d6f50e9abb3194f9a1ffd5797ec7b
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-21 15:20:53 +0330

    Making the corresponding page of each slide | ساخت صحفه  مربوطه هر اسلاید

--- Articles/urls.py
+++ Articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Articles_view , detail_alert
+from .views import Articles_view , detail_alert , detail_slider
 
 app_name = "Articles_app_name"
 urlpatterns = [
     path('', Articles_view , name="Articles_name"),
     path('alert/<slug:slug>', detail_alert, name="alert_name"),
+    path('slider/<slug:slug>', detail_slider, name="slider_name"),
 ]
 
