1d5be7abe8184c91576990e4aeba39ecd99ec122
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-21 21:06:11 +0330

    Add =>   .order_by('-publish_ArticleHome')

--- MainPage/views.py
+++ MainPage/views.py
@@ -8,9 +8,9 @@ def MainPage_view(request):
          "Alertspagemain" : AlertModelMainPage.objects.filter(status_alert="p"), # شیش تای آخر رو نشون میده
         #  "Alertspagemain" : AlertModelMainPage.objects.published(), # میتونی اینجوری هم بنویسی
         # کتاب ها        
-         "Bookspagemain" : BookHomeModelMainPage.objects.filter(status_BookHome="p")[:6], # شیش تای آخر رو نشون میده
+         "Bookspagemain" : BookHomeModelMainPage.objects.filter(status_BookHome="p").order_by('-publish_BookHome')[:6], # شیش تای آخر رو نشون میده
         # مقاله ها        
-         "Articlspagemain" : ArticleHomeModelMainPage.objects.filter(status_ArticleHome="p")[:6], # شیش تای آخر رو نشون میده
+         "Articlspagemain" : ArticleHomeModelMainPage.objects.filter(status_ArticleHome="p").order_by('-publish_ArticleHome')[:6], # شیش تای آخر رو نشون میده
 
      }
     return render(request, "MainPage/index.html", cotext)
