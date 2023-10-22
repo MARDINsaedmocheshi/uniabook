f18a3e411b52f57c9a8806bde6108545dcdf35fe
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-22 10:14:29 +0330

    Creating specific tags (inclusion tag) - related to categories | ایجاد تگ های اختصاصی (اینکلوژن تگ) -مربوط به دسته بندی ها

--- Articles/templatetags/base_tags.py
+++ Articles/templatetags/base_tags.py
@@ -1,7 +1,18 @@
 from django import template
+from ..models import Category
+
 
 register = template.Library()
 
 @register.simple_tag
 def Title(data= "دانشگاه کتاب و مقاله"):     # این یک مقدار پیش فرض یا دیفالت است که اگر چیزی ننویسیم همین را برمیگرداند 
     return data
+
+
+@register.inclusion_tag("Articles/partials/category_navbar.html")
+def category_navbar():# توی فایل index_Articles.html خط 324 ازش استفاده کرده ام
+    return {
+        # دسته بندی ها
+         "CATEGORY" : Category.objects.filter(status_category=True),
+
+    }
