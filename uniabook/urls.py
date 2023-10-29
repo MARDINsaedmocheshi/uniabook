4d29f71bbcae8e60a6deb13c5e3b6bd03a3123b9
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-10-29 20:28:30 +0330

    Designing and setting the login page | طراحی و تنظیم صفحه لاگین

--- uniabook/urls.py
+++ uniabook/urls.py
@@ -17,3 +17,4 @@ urlpatterns = [
 
 # فقط در حالت دیباگ به کار میاد برای فایل میدیاها است
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
