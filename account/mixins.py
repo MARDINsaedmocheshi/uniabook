9987e5c69d0a58994234fcc34134b70fa97c4bd3
MARDINsaedmocheshi <saedmocheshimardin@gmail.com>
2023-11-14 19:37:20 +0330

    Determining the status of the article by the authors | تعیین وضعیت مقاله توسط نویسندگان

--- account/mixins.py
+++ account/mixins.py
@@ -11,7 +11,7 @@ class fieldsMixin():
                             ]
         elif request.user.is_author: #نویسندگان
             self.fields = [
-                'image_Article' , 'File_Article' , 'title_Article' ,'is_special', 'slug_Article', 'Abstract_Article', 'Key_word_Article', 'Introduction_Article', 'Body_or_text_Article', 'Result_Article', 'References_Article', 'category_Article', 'Article_type_Article', 'is_sale_Article'
+                'image_Article' , 'File_Article' , 'title_Article' ,'is_special', 'slug_Article', 'Abstract_Article', 'Key_word_Article', 'Introduction_Article', 'Body_or_text_Article', 'Result_Article', 'References_Article', 'category_Article', 'Article_type_Article', 'is_sale_Article', 'status_Article'
                             ]
         else:
             raise Http404("شما نمیتوانید این صفحه را ببینید")
@@ -24,7 +24,11 @@ class FormValidMixin():
         else:
             self.obj = form.save(commit=False)
             self.obj.author =  self.request.user
-            self.obj.status_Article = 'd'
+            # self.obj.status_Article = 'd'
+            # if not self.obj.status_Article in ['d','i']:
+            #     self.obj.status_Article = 'd'
+            if not self.obj.status_Article == 'i':
+                self.obj.status_Article = 'd'
         return super().form_valid(form)                             
 
 
@@ -64,9 +68,13 @@ class superuserAccessMixin():
 
 class AthorsAccessMixin():
     def dispatch(self , request , *args , **kwargs):
+        if request.user.is_authenticated:
 
-        if request.user.is_superuser or request.user.is_author:
-            return super().dispatch(request , *args , **kwargs)
+            if request.user.is_superuser or request.user.is_author:
+                return super().dispatch(request , *args , **kwargs)
+            else:
+                return redirect("ACCOUNT:profile")
         else:
-            return redirect("ACCOUNT:profile")
+            return redirect("ACCOUNT:login")
+
 # در اینجا کل نویسندگان رو بررسی میکنیم که ایا دسترسی دارن یا نه
